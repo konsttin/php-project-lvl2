51e57e2c3146f12e0a7e96724c9e122979209623
Konstantin Glebov <5449277@gmail.com>
2022-04-28 17:52:55 +0300

    Add tests

--- src/Differ.php
+++ src/Differ.php
@@ -1,30 +1,29 @@
 <?php
 
-namespace src\differ;
+namespace src\Differ;
 
 function genDiff($firstFile, $secondFile)
 {
     $file1 = json_decode($firstFile, true, 512, JSON_THROW_ON_ERROR);
     $file2 = json_decode($secondFile, true, 512, JSON_THROW_ON_ERROR);
 
-    $result = [];
-
-    foreach ($file1 as $key1 => $value1) {
-        foreach($file2 as $key2 => $value2) {
-            if (!array_key_exists($key1, $file2)) {
-                $result[] = '- ' . $file1[$key1];
+    $result = array_reduce($file1, function ($acc, $value1) use ($file1, $file2) {
+        foreach ($file2 as $key2 => $value2) {
+            if (!array_key_exists(key($value1), $file2)) {
+                $acc = '- ' . $value1;
             }
             if (!array_key_exists($key2, $file1)) {
-                $result[] = '+ ' . $file2[$key2];
+                $acc = '+ ' . $value2;
             }
-            if ($key1 === $key2 && $value1 === $value2) {
-                $result[] = $value1;
+            if (key($value1) === $key2 && $value1 === $value2) {
+                $acc = $value1;
             }
-            if ($key1 === $key2 && $value1 !== $value2) {
-                $result[] = '- ' . $value1 . "\n" . '+ ' . $file1[$key2];
+            if (key($value1) === $key2 && $value1 !== $value2) {
+                $acc = '- ' . $value1 . "\n" . '+ ' . $file1[$key2];
             }
         }
-        print_r($result);
-        return $result;
-    }
+        return $acc;
+    }, '');
+    print_r($result);
+    return $result;
 }
---
+++ tests/TestDiffer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function src\Differ\genDiff;
+
+class TestDiffer extends TestCase
+{
+    public function testDiffer()
+    {
+        $firstFile = '{
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}';
+
+        $secondFile = '{
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+}';
+        $this->assertEquals('{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}', genDiff($firstFile, $secondFile));
+    }
+}
