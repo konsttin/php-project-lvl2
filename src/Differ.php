407e7da91b4772dd0714d526f53abaabe1ce0ac5
Konstantin Glebov <5449277@gmail.com>
2022-06-09 17:29:13 +0300

    Edit function genDiff

--- src/Differ.php
+++ src/Differ.php
@@ -7,23 +7,39 @@ function genDiff($firstFile, $secondFile)
     $file1 = json_decode($firstFile, true, 512, JSON_THROW_ON_ERROR);
     $file2 = json_decode($secondFile, true, 512, JSON_THROW_ON_ERROR);
 
-    $result = [];
+    //$result = [];
 
-    foreach ($file1 as $key1 => $value1) {
-        foreach($file2 as $key2 => $value2) {
-            if (!array_key_exists($key1, $file2)) {
-                $result[] = '- ' . $key1 . ': ' . $value1;
+    $merge = array_merge($file1, $file2);
+    $keys = array_keys($merge);
+
+    $mapped = array_map(callback: static function ($key) use ($merge, $file1, $file2) {
+        if (!array_key_exists($key, $file2)) {
+            if (is_bool($merge[$key])) {
+                $merge[$key] = $merge[$key] ? 'true' : 'false';
             }
-            if (!array_key_exists($key2, $file1)) {
-                $result[] = '+ ' . $key2 . ': ' . $value2;
-            }
-            if ($key1 === $key2 && $value1 === $value2) {
-                $result[] = '  ' . $key1 . ': ' . $value1;
-            }
-            if ($key1 === $key2 && $value1 !== $value2) {
-                $result[] = '- ' . $key1 . ': ' . $value1 . "\n " . '+ ' . $key2 . ': ' . $value2;
-            }
+            return '- ' . $key . ': ' . $merge[$key];
         }
-    }
-    return $result;
+        if (!array_key_exists($key, $file1)) {
+            if (is_bool($merge[$key])) {
+                $merge[$key] = $merge[$key] ? 'true' : 'false';
+            }
+            return '+ ' . $key . ': ' . $merge[$key];
+        }
+        if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
+            if (is_bool($merge[$key])) {
+                $merge[$key] = $merge[$key] ? 'true' : 'false';
+            }
+            return '  ' . $key . ': ' . $merge[$key];
+        }
+        if (array_key_exists($key, $file1) && array_key_exists($key, $file2) && $file1[$key] !== $file2[$key]) {
+            if (is_bool($file1[$key])) {
+                $file1[$key] = $file1[$key] ? 'true' : 'false';
+            }
+            if (is_bool($file2[$key])) {
+                $file2[$key] = $file2[$key] ? 'true' : 'false';
+            }
+            return '- ' . $key . ': ' . $file1[$key] . "\n " . '+ ' . $key . ': ' . $file2[$key];
+        }
+    }, array: $keys);
+    return $mapped;
 }
