6cffe7049a71ef2731a8d3bc7ccf9c65a19cba36
Konstantin Glebov <5449277@gmail.com>
2022-07-06 17:08:27 +0300

    Edit test file

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,6 +10,6 @@ class DifferTest extends TestCase
     {
         $result = file_get_contents(__DIR__ . '/../tests/fixtures/result');
 
-        $this->assertEquals($result, genDiff('fixtures/file1.json', 'fixtures/file2.json'));
+        $this->assertEquals($result, genDiff('/tests/fixtures/file1.json', '/tests/fixtures/file2.json'));
     }
 }
