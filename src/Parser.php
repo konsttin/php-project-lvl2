af0f76a5275256a51fd2c26bba04f6b38f463e2e
Konstantin Glebov <5449277@gmail.com>
2022-08-10 17:21:00 +0300

    Add Differ & Stylish

--- src/Parser.php
+++ src/Parser.php
@@ -8,29 +8,49 @@ function parser(mixed $decodedFirstFile, mixed $decodedSecondFile): string
     $keys = array_keys($merge);
     sort($keys);
 
-    $mapped = array_map(callback: static function ($key) use ($merge, $decodedFirstFile, $decodedSecondFile) {
+    $diff = array_map(callback: static function ($key) use ($merge, $decodedFirstFile, $decodedSecondFile) {
         if (is_bool($merge[$key])) {
             $merge[$key] = $merge[$key] ? 'true' : 'false';
         }
-        if (!array_key_exists($key, $decodedSecondFile)) {
-            return '  - ' . $key . ': ' . $merge[$key];
-        }
-        if (!array_key_exists($key, $decodedFirstFile)) {
-            return '  + ' . $key . ': ' . $merge[$key];
+        if (is_array($decodedFirstFile[$key]) && is_array($decodedSecondFile[$key])) {
+            return ['node' =>
+                ['status' => 'unchanged',
+                'key' => $key,
+                'value' => parser($decodedFirstFile[$key], $decodedSecondFile[$key])]
+            ];
         }
         if ($decodedFirstFile[$key] === $decodedSecondFile[$key]) {
-            if (is_array($decodedFirstFile[$key]) && is_array($decodedSecondFile[$key])) {
-                return parser($decodedFirstFile[$key], $decodedSecondFile[$key]);
-            }
-            return '    ' . $key . ': ' . $merge[$key];
+            return ['sheet' => ['status' => 'unchanged', 'key' => $key, 'value' => $merge[$key]]];
         }
-        return '  - ' . $key . ': ' . $decodedFirstFile[$key] . "\n" . '  + ' . $key . ': ' . $decodedSecondFile[$key];
+        if (!array_key_exists($key, $decodedSecondFile)) {
+            return ['sheet' => ['status' => 'removed', 'key' => $key, 'value' => $merge[$key]]];
+        }
+        if (!array_key_exists($key, $decodedFirstFile)) {
+            return ['sheet' => ['status' => 'added', 'key' => $key, 'value' => $merge[$key]]];
+        }
+        return [
+            ['sheet' => ['status' => 'removed', 'key' => $key, 'value' => $decodedFirstFile[$key]]],
+            ['sheet' => ['status' => 'added', 'key' => $key, 'value' => $decodedSecondFile[$key]]]
+        ];
     }, array: $keys);
-    return stylish($mapped);
+    return stylish($diff);
 }
 
-function stylish(mixed $mapped): string
+function stylish(array $fileDiff): string
 {
+    $mapped = array_map(callback: static function ($key) use ($fileDiff) {
+        if ($key === 'node') {
+            return '    ' . $key . ': ' . stylish($fileDiff[$key]);
+        }
+        if (key($fileDiff) === 'sheet' && $fileDiff[$key][1] = 'removed') {
+            return '  - ' . $key . ': ' . $fileDiff[$key];
+        }
+        if (key($fileDiff) === 'sheet' && $fileDiff[$key][1] = 'added') {
+            return '  + ' . $key . ': ' . $fileDiff[$key];
+        }
+        return '    ' . $key . ': ' . $fileDiff[$key];
+    }, array: $fileDiff);
+
     $string = implode("\n", $mapped);
     $result = '{' . "\n" . $string . "\n" . '}';
     print_r($result);
